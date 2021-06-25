8eb37cf6f9b5753c4c343143ab7f94780ba48908
Harsh J <whizkid@gmail.com>
2021-06-24 18:12:33 -0700

    code with divide by 0 error and index error

--- code/bfr.py
+++ code/bfr.py
@@ -18,12 +18,12 @@ class HCluster:
         self.num_seeds = num_seeds
 
     @staticmethod
-    def get_centroids(points, num_clusters):
+    def get_centroids(pwi, num_clusters):
         centroids = [None] * num_clusters
         used_points = set()
-        N = len(points)
+        N = len(pwi)
         init_idx = random.randint(0, N)
-        centroids[0] = points[init_idx]
+        centroids[0] = pwi[init_idx][1]
         used_points.add(init_idx)
 
         for i in range(1, num_clusters):
@@ -31,8 +31,8 @@ class HCluster:
             new_centroid = None
             used_idx = None
 
-            for idx, point in enumerate(points):
-                if not point in used_points:
+            for idx, point in pwi:
+                if not idx in used_points:
                     min_dist = Utils.INF
                     for x in range(i):
                         dist = Utils.euclidean_distance(
@@ -53,7 +53,7 @@ class HCluster:
         # Sum of squares distance from points to centers
         inertia = 0
 
-        for idx, p in enumerate(points):
+        for idx, p in points:
             inertia += pow(Utils.euclidean_distance(p, centers[labels[idx]]), 2)
         return inertia
 
@@ -77,15 +77,15 @@ class HCluster:
             centers_new[cidx] = dims
         return centers_new
 
-    def single_iteration(self, points, centers, centers_new):
-        labels = [None] * len(points)
-        num_dims = len(points[0])
+    def single_iteration(self, pwi, centers, centers_new):
+        labels = {}
+        num_dims = len(pwi[0][1])
         
         # Updates stores the SUM of points along each dimension
         updates = defaultdict(lambda: [0] * num_dims)
         cluster_counts = defaultdict(int)
 
-        for pidx, point in enumerate(points):
+        for pidx, point in pwi:
             min_dist = Utils.INF
 
             for idx, centroid in enumerate(centers):
@@ -99,25 +99,25 @@ class HCluster:
         centers_new = HCluster.update_centroids(centers, centers_new, updates, cluster_counts)
         return centers, centers_new, labels
         
-    def run(self, points, centers):
+    def run(self, pwi, centers):
         centers_new = [None] * len(centers)
 
         for i in range(self.max_iterations):
             print("Running iteration #: {}".format(i + 1))
-            centers_new, centers, labels = self.single_iteration(points, centers, centers_new)
-        inertia = HCluster.get_inertia(centers_new, points, labels)
+            centers_new, centers, labels = self.single_iteration(pwi, centers, centers_new)
+        inertia = HCluster.get_inertia(centers_new, pwi, labels)
 
         return centers, labels, inertia
 
-    def fit(self, points):
+    def fit(self, pwi):
         best_inertia = None
 
         for _ in range(self.num_seeds):
             st = time.time()
-            centers = self.get_centroids(points, self.num_clusters)
+            centers = self.get_centroids(pwi, self.num_clusters)
             print("Time to get centroids: {}".format(time.time() - st))
 
-            centers, labels, inertia = self.run(points, centers)
+            centers, labels, inertia = self.run(pwi, centers)
 
             if best_inertia is None or inertia < best_inertia:
                 best_labels = labels
@@ -127,6 +127,7 @@ class HCluster:
         self.cluster_centers = best_centers
         self.labels = best_labels
         self.inertia = best_inertia
+
 
 class Utils:
     INF = float('inf')
@@ -169,6 +170,7 @@ class DiscardSet:
             stds[i] = math.sqrt(self.dsqsums[i] / self.num_points - pow(self.dsums[i] / self.num_points, 2))
         return stds
 
+
 class Runner:
     def __init__(self) -> None:
         self.input_path = sys.argv[1]
@@ -195,46 +197,61 @@ class Runner:
             if label in singleton_clusters:
                 pass
 
-    def init_DSs(self, points, labels, centers):
-        N = len(points)
+    def init_DSs(self, pwi, labels, centers):
+        N = len(pwi)
         ds_points = [[] for _ in range(self.num_clusters)]
 
         for i in range(N):
-            point = points[i]
-            label = labels[i]
+            pidx = pwi[i][0]
+            point = pwi[i][1]
+            label = labels[pidx]
             ds_points[label].append(point)
         
         for i in range(self.num_clusters):
             self.discard_sets.append(DiscardSet(ds_points[i], centers[i]))
 
-    def init_sets(self, pointsRDD, file_path, num_clusters):
-        points_with_idx = pointsRDD.collectAsMap()
-        points = points_with_idx.values()
-        num_points = len(points)
+    def init_sets(self, pointsRDD):
+        pwi = pointsRDD.collect()
+        num_points = len(pwi)
         fraction = 0.2
-        sample = math.ceil(num_points * fraction)
-        points_sample = points[:sample]
+        sample_idx = math.ceil(num_points * fraction)
+        points_sample = pwi[:sample_idx]
         
-        clusters = HCluster(num_clusters * 3, num_seeds=1, num_iterations=1)
+        clusters = HCluster(self.num_clusters * 3, num_seeds=1, num_iterations=5)
         clusters.fit(points_sample)
         
-        ctr = Counter(clusters.labels)
+        ctr = Counter(clusters.labels.values())
 
         outlier_labels = set()
 
-        for k, v in ctr:
+        for k, v in ctr.items():
             if v == 1:
                 outlier_labels.add(k)
         
-        inlier_points = {}
-        outlier_points = {}
-        for k, v in points_with_idx:
-            if k not in outlier_labels:
-                inlier_points[k] = v
-            else:
-                outlier_labels[k] = v
-        for eidx, (idx, point) in enumerate(inlier_points.items()):
+        inlier_points = []
+        outlier_points = []
 
+        for point_with_idx in points_sample:
+            idx = point_with_idx[0]
+
+            if clusters.labels[idx] not in outlier_labels:
+                inlier_points.append(point_with_idx)
+            else:
+                outlier_points.append(point_with_idx)
+        
+        inlier_points.extend(pwi[sample_idx:])
+
+        print("Number of inlier points + rest: {}".format(len(inlier_points)))
+        clustering = HCluster(self.num_clusters, num_seeds=1, num_iterations=5)
+        clustering.fit(inlier_points)
+
+        print("Number of label keys: {}".format(len(clustering.labels)))
+        self.init_DSs(pwi, clustering.labels, clustering.cluster_centers)
+
+        # Write initial assignment
+        for idx, _ in inlier_points:
+            label = clustering.labels[idx]
+            self.out_dict[idx] = label
 
     @staticmethod
     def assign_to_ss(point, summarized_sets, alpha):
@@ -256,8 +273,6 @@ class Runner:
 
         files = list(sorted(os.listdir(self.input_path)))
         print("Files: {}".format(files))
-        
-        self.init_DSs(init_points, clusters.labels, clusters.cluster_centers)
 
         for idx, file_path in enumerate(files):
             print("Processing index: {}/{}".format(idx + 1, len(files)))
@@ -265,15 +280,15 @@ class Runner:
             
             # For the first round, we want to do some extra stuff
             if idx == 0:
-                pass
-
-            dss = self.discard_sets
-            assigned = pointsRDD.map(lambda idx_point: (idx_point[0], Runner.assign_to_ss(idx_point[1], dss, alpha=2)))
-            rem_points = assigned.filter(lambda row: row[1] == None)
-            ds_assigned = assigned.filter(lambda row: row[1] != None)
-            self.out_dict.update(ds_assigned.collectAsMap())
-            rem_minus_one = rem_points.mapValues(lambda val: -1)
-            self.out_dict.update(rem_minus_one.collectAsMap())
+                self.init_sets(pointsRDD)
+            else:
+                dss = self.discard_sets
+                assigned = pointsRDD.map(lambda idx_point: (idx_point[0], Runner.assign_to_ss(idx_point[1], dss, alpha=2)))
+                rem_points = assigned.filter(lambda row: row[1] == None)
+                ds_assigned = assigned.filter(lambda row: row[1] != None)
+                self.out_dict.update(ds_assigned.collectAsMap())
+                rem_minus_one = rem_points.mapValues(lambda val: -1)
+                self.out_dict.update(rem_minus_one.collectAsMap())
             print("Number of assigned points in out dict: {}".format(len(self.out_dict)))
         
         with open(self.cluster_out, "w+") as f:
